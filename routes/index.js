ff2db9c4bff17facf727a00c09ac38c8957794bf
nakano16180 <nakano16180@gmail.com>
2020-03-26 20:47:47 +0900

    postデータをファイルに吐き出せるようにした

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const fs = require('fs');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.json({
-    message:"Hello,world"
-  });
+  const jsonObject = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+  res.json(jsonObject);
 });
 
 router.post('/', (req, res) => {
+  fs.writeFileSync('./data.json', JSON.stringify(req.body));
   res.json({
     message: "Post Success!"
   })
